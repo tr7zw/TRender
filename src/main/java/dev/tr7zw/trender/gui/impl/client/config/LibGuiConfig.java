c012f3cc747ce4831014a3da2b04be6eddc86621
tr7zw <tr7zw@live.de>
2025-06-02 21:54:47 +0200

    Default to the correct style

--- src/main/java/dev/tr7zw/trender/gui/impl/client/config/LibGuiConfig.java
+++ src/main/java/dev/tr7zw/trender/gui/impl/client/config/LibGuiConfig.java
@@ -5,5 +5,9 @@ import dev.tr7zw.trender.gui.impl.client.style.GuiStyle;
 public class LibGuiConfig {
 
 
-    public GuiStyle style = GuiStyle.MODERN;
+    //#if MC >= 12006
+    public GuiStyle style = GuiStyle.VANILLA_MODERN;
+    //#else
+    //$$ public GuiStyle style = GuiStyle.VANILLA_OLD;
+    //#endif
 }
